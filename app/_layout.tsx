b9c7dac714f7e1cd61b24a31b0575954d9952d3c
Ayush Trivedi <ayushtrivedi890@gmail.com>
2024-09-06 16:08:12 +0530

    login page setup start

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,9 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
 
   useFonts({
-    
+    'outfit':require('./../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium':require('./../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf'),
   })
 
   return (
