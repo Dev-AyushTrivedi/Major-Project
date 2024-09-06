285dcafded8e13ebde033406831e9fd64d2ce843
Ayush Trivedi <ayushtrivedi890@gmail.com>
2024-09-06 13:01:42 +0530

    Starting working on  The Project 🚀

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+
+  useFonts({
+    
+  })
+
   return (
     <Stack>
       <Stack.Screen name="index" />
