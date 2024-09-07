872d52b8f0bc7e9d2b205f66dcc87fe098f8dee7
Ayush Trivedi <ayushtrivedi890@gmail.com>
2024-09-07 20:35:11 +0530

    Authentication Starting

---
+++ app/auth/sign-in/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SignIn() {
+  // at 40 mintues 
+  return (
+    <View style={{
+      padding:25,
+    }}>
+
+    </View>
+  )
+}
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,14 +1,17 @@
 import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { Colors } from '@/constants/Colors'
+import { useRouter } from 'expo-router'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function Login() {
+  const router=useRouter();
   return (
     <View>
         <Image source={require('./../assets/images/Login.jpg')}
           style={{
             width: '100%',
-            height:450
+            height:520
           }}
         />
 
@@ -27,14 +30,16 @@ export default function Login() {
             marginTop:20
           }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero blanditiis minus sequi id maxime illum possimus, harum obcaecati, quasi quisquam molestias quos itaque, a accusantium.</Text>
           
-          
-          <View style={styles.button}>
+
+          <TouchableOpacity style={styles.button}
+          onPress={()=>router.push('auth/sign-in')}
+          >
             <Text style={{color:Colors.WHITE,
               fontFamily:'outfit',
               fontSize:17,
-              textAlign:'center'}}>Sign in With Google
+              textAlign:'center'}}>Get Started
             </Text>
-          </View>
+          </TouchableOpacity>
         </View>
     </View>
   )
