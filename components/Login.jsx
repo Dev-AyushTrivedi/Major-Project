29dc3db16e2794f0b02f293c9d793e2c077e17d9
Ayush Trivedi <ayushtrivedi890@gmail.com>
2024-09-07 06:01:23 +0530

    making the part slightly round and changing the font size

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,10 +1,34 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
 
 export default function Login() {
   return (
     <View>
-        <Image source={require('./../assets/images/Login.jpg')}/>
+        <Image source={require('./../assets/images/Login.jpg')}
+          style={{
+            width: '100%',
+            height:450
+          }}
+        />
+        <View style={styles.container}>
+          <Text style={{
+            fontSize:28,
+            fontFamily: 'outfit-bold',
+            textAlign:'center',
+          }}>AI Travel Planner</Text>
+        </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:Colors.WHITE,
+    marginTop:-20,
+    borderTopLeftRadius:30,
+    borderTopRightRadius:30,
+    height:'100%',
+    padding:15,
+  }
+})
