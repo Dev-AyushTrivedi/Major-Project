cc2c34aff577ab0792453f9de56163a74654a1db
Ayush Trivedi <ayushtrivedi890@gmail.com>
2024-09-08 12:10:32 +0530

    Sign in page setup

--- app/auth/sign-in/index.js
+++ app/auth/sign-in/index.js
@@ -1,13 +1,46 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Colors } from './../../../constants/Colors';
 
 export default function SignIn() {
-  // at 40 mintues 
+  const navigation=useNavigation();
+
+  useEffect(()=>{
+    navigation.setOptions({
+      HeaderShown:false
+    })
+  },[])
   return (
     <View style={{
       padding:25,
+      paddingTop:80,
+      backgroundColor:Colors.WHITE,
+      height:'100%'
     }}>
+      <Text style={{
+        fontFamily:'outfit-bold',
+        fontSize:30
+      }}>Let's Sign you In</Text>
+      <Text style={{
+        fontFamily:'outfit',
+        fontSize:30,
+        color:Colors.GRAY,
+        marginTop:20
+      }}>Welcome back</Text>
+      <Text style={{
+        fontFamily:'outfit',
+        fontSize:30,
+        color:Colors.GRAY,
+        marginTop:20
+      }}>You've been missed</Text>
 
+      <View>
+        <Text>
+          {/* at 44.41 */}
+        </Text>
+      </View>
     </View>
   )
 }
