88012667c784afe3492e150888c113a079a020b7
Ayush Trivedi <ayushtrivedi890@gmail.com>
2024-09-23 16:05:52 +0530

    new trip card added

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,10 +1,35 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-
+import React, { useState } from 'react'
+import {Colors} from './../../constants/Colors'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import StartNewTripCard from '../../components/MyTrips/StartNewTripCard';
 export default function MyTrip() {
+    const [userTrips,setUserTrips]=useState({});
+
     return (
-        <View>
-            <Text> My Trip </Text>
+        <View style={{
+            padding:25,
+            paddingTop:55,
+            backgroundColor:Colors.WHITE,
+            height:'100%',
+        }}>
+            <View style={{
+                display:'flex',
+                flexDirection:'row',
+                alignContent:'center',
+                justifyContent:'space-between'
+            }}>
+                <Text style={{
+                    fontFamily:'outfit',
+                    fontSize:35  
+                }}>My Trip </Text>
+                <Ionicons name="add-circle" size={58} color="black" />
+            </View>
+
+            {userTrips?.length==0?
+              <StartNewTripCard/>
+              :null
+            }
         </View>
     )
 }
